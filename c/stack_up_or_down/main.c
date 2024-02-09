b4df5f41c9414886322cfabf8a0e97338faff0ac
BlakeFreer <freerb@mcmaster.ca>
2024-02-08 20:16:13 -0500

    updates docs and fixes a bug

--- c/stack_up_or_down/main.c
+++ c/stack_up_or_down/main.c
@@ -4,27 +4,36 @@
  * @brief Solve the interview problem posed by LowLevelLearning in the video
  * https://www.youtube.com/watch?v=V2h_hJ5MSpY
  * The problem is to write a program that determines if the stack grows up or
- * down,
+ * down.
  **/
 
 #include <stdio.h>
 
 typedef unsigned long long ptr_t;
 
+/// @brief Allocate a variable on the stack.
+/// @return Address of that variable
 ptr_t inner() {
 	int x = 0;
 	
 	return (ptr_t)&x;
 }
 
-int main(int argc, char** argv) {
+int main(void) {
+	int temp = 0;
 
-	int x = 0;
-	int y = inner();
+	// Get the address of the arbitrary variable
+	ptr_t x = (ptr_t)&temp;
+
+	// Get the address of a variable from a function pushed onto the stack
+	// after main()
+	ptr_t y = inner();
 
 	if(y > x) {
+		// inner() variable has a greater address
 		printf("Stack grows up!\n");
 	} else {
+		// inner() variable has a lower address
 		printf("Stack grows down!\n");
 	}
 
