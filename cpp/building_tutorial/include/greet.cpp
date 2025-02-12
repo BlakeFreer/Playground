15107a347598a2cae678329d59b1cb4fc72b9e45
BlakeFreer <freerb@mcmaster.ca>
2025-02-12 15:45:15 -0500

    Basic source/header separation & Makefile

---
+++ cpp/building_tutorial/include/greet.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "greet.hpp"
+
+void greet(std::string name) {
+    std::cout << "G'day, " << name << std::endl;
+}
---
+++ cpp/building_tutorial/main.cc
@@ -0,0 +1,8 @@
+#include "greet.hpp"
+
+int main() {
+    greet("Sam");
+    greet("Blake");
+
+    return 0;
+}
