80e2aff5793704c649b86de93116c70222092ad3
Blake Freer <bkfreer10@gmail.com>
2025-08-21 21:58:52 -0700

    tcp server

---
+++ rust/tcp/src/main.rs
@@ -0,0 +1,91 @@
+use http_body_util::{BodyExt, Empty, Full, combinators::BoxBody};
+use hyper::{
+    Method, Request, Response, StatusCode,
+    body::{Body, Bytes, Frame},
+    server::conn::http1,
+    service::service_fn,
+};
+use hyper_util::rt::TokioIo;
+use std::{convert::Infallible, net::SocketAddr};
+use tokio::net::TcpListener;
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World"))))
+}
+
+async fn echo(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(full("Try POSTing data to /echo"))),
+        (&Method::POST, "/echo") => Ok(Response::new(req.into_body().boxed())),
+        (&Method::POST, "/echo/uppercase") => {
+            let frame_stream = req.into_body().map_frame(|frame| {
+                let frame = if let Ok(data) = frame.into_data() {
+                    data.iter()
+                        .map(|byte| byte.to_ascii_uppercase())
+                        .collect::<Bytes>()
+                } else {
+                    Bytes::new()
+                };
+
+                Frame::data(frame)
+            });
+            Ok(Response::new(frame_stream.boxed()))
+        }
+        (&Method::POST, "/echo/reversed") => {
+            let upper = req.body().size_hint().upper().unwrap_or(u64::MAX);
+            if upper > 1024 * 64 {
+                let mut resp = Response::new(full("Body to big"));
+                *resp.status_mut() = hyper::StatusCode::PAYLOAD_TOO_LARGE;
+                return Ok(resp);
+            }
+
+            let whole_body = req.collect().await?.to_bytes();
+            let reverse_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
+
+            Ok(Response::new(full(reverse_body)))
+        }
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Serving on {addr}");
+    println!("Try running\n\tcurl {addr}/echo -d \"Hello World\"");
+
+    let listener = TcpListener::bind(addr).await?;
+
+    loop {
+        let (stream, port) = listener.accept().await?;
+
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            if let Err(e) = http1::Builder::new()
+                // .serve_connection(io, service_fn(hello))
+                .serve_connection(io, service_fn(echo))
+                .await
+            {
+                eprintln!("Error sering connection on port {port}. {e:?}");
+            }
+        });
+    }
+}
