f3ec650490a927aba65ba1afb827468824e78932
Blake Freer <bkfreer10@gmail.com>
2025-10-20 17:31:57 -0400

    compile gRPC with platformio

---
+++ cpp/grpc-platformio/gen_proto.py
@@ -0,0 +1,33 @@
+Import("env")
+import subprocess
+import os
+import glob
+
+build_dir = env.subst("$BUILD_DIR")
+generated_src_dir = os.path.join(build_dir, "protoc", "generated-src")
+generated_build_dir = os.path.join(build_dir, "protoc", "generated-build")
+
+os.makedirs(generated_src_dir, exist_ok=True)
+os.makedirs(generated_build_dir, exist_ok=True)
+
+grpc_path = os.path.expanduser(env.GetProjectOption("custom_grpc_path"))
+
+proto_src = glob.glob("proto/*.proto")
+protoc = os.path.join(grpc_path, "bin", "protoc")
+
+subprocess.run(
+    [
+        protoc,
+        f"--cpp_out={generated_src_dir}",
+        *proto_src,
+    ],
+    check=True,
+)
+
+env.BuildSources(generated_build_dir, generated_src_dir)
+env.Append(CPPPATH=[generated_src_dir])
+
+pkg_path = os.path.join(grpc_path, "lib", "pkgconfig")
+env.ParseConfig(f"pkg-config grpc++ --cflags --libs --with-path={pkg_path}")
+env.ParseConfig(f"pkg-config protobuf --cflags --libs --with-path={pkg_path}")
+env.ParseConfig(f"pkg-config upb --cflags --libs --with-path={pkg_path}")
--- cpp/grpc-platformio/src/main.cpp
+++ cpp/grpc-platformio/src/main.cpp
@@ -1,7 +1,14 @@
 #include <grpcpp/grpcpp.h>
 #include <iostream>
+#include "proto/greeter.pb.h"
 
 int main() {
     std::cout << "gRPC version: " << grpc::Version() << std::endl;
+
+    HelloRequest h;
+    h.set_name("Blake");
+
+    std::cout << h.name() << std::endl;
+
     return 0;
 }
